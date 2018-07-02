4a65ee6024617d027ba69e4d997de018a260f9ac
kamal <kamalpradhan95@outlook.com>
2018-07-02 21:22:14 +0530

    basic scrapper pulling all horse names from static pages

--- main.php
+++ main.php
@@ -2,5 +2,24 @@
 
 //Loading the html files dynamically
 $html = file_get_contents('http://www.alltidhest.org/Horses/Page/1');
-echo $html;
+
+//echo $html;
+
+//Creating a DOM Object
+$dom = new domDocument;
+@$dom->loadHTML($html);
+$dom->preserveWhiteSpace = false;
+
+$tables = $dom->getElementsByTagName('table');
+
+var_dump($tables);
+
+$rows = $tables->item(0)->getElementsByTagName('tr');
+
+foreach ($rows as $row) {
+        $cols = $row->getElementsByTagName('td');
+        echo $cols[2]->nodeValue;
+}
+
+
 ?>
