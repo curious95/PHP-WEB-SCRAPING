cc6ba11b412ad3c5c966536effd9598fd6adf3ee
kamal <kamalpradhan95@outlook.com>
2018-07-02 21:49:49 +0530

    Automated the page to work untill no tables are found

--- main.php
+++ main.php
@@ -12,24 +12,31 @@ $dom->preserveWhiteSpace = false;
 
 $tables = $dom->getElementsByTagName('table');
 
-var_dump($tables);
 
-$rows = $tables->item(0)->getElementsByTagName('tr');
 
-foreach ($rows as $row) {
-        $cols = $row->getElementsByTagName('td');
-        
+for($i=0;$i<999999;$i++){
 
-        // fetching all the required columns
-        $horseID = trim($cols[1]->nodeValue);
-        $horseName = trim($cols[2]->nodeValue);
-        $regNo = trim($cols[3]->nodeValue);
-        $age = trim($cols[4]->nodeValue);
-        $sex = trim($cols[5]->nodeValue);
-        $color = trim($cols[6]->nodeValue);
-        $country = trim($cols[7]->nodeValue);
+	$rows = $tables->item(0)->getElementsByTagName('tr');
+	if($rows->length <=1){
+		break;
+	}
 
-        echo $horseID.'   '.$horseName.'   '.$regNo.'   '.$age.'   '.$sex.'   '.$color.'   '.$country."\n";
+
+	foreach ($rows as $row) {
+	        $cols = $row->getElementsByTagName('td');
+	        
+
+	        // fetching all the required columns
+	        $horseID = trim($cols[1]->nodeValue);
+	        $horseName = trim($cols[2]->nodeValue);
+	        $regNo = trim($cols[3]->nodeValue);
+	        $age = trim($cols[4]->nodeValue);
+	        $sex = trim($cols[5]->nodeValue);
+	        $color = trim($cols[6]->nodeValue);
+	        $country = trim($cols[7]->nodeValue);
+
+	        echo $horseID.'   '.$horseName.'   '.$regNo.'   '.$age.'   '.$sex.'   '.$color.'   '.$country."\n";
+	}
 }
 
 
